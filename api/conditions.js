60b49c30379411b57542b27dd8c95be4087646d1
CN087005 <jay.liang@amway.com>
2024-04-20 16:32:03 +0800

    xx

--- api/conditions.js
+++ api/conditions.js
@@ -13,8 +13,10 @@ const {conditionsCore} = require('../core/conditionsCore');
 // };
 
 module.exports = async (req, res) => {
+  const { conditions, conditionTypes } = req.body;
   try {
-    res.status(200).json({"result":"true"});
+    const result = await conditionsCore(conditions, conditionTypes);
+    res.status(200).json(result);
     res.end(); // 关闭连接
   } catch (error) {
     res.status(500).send(error.message);
